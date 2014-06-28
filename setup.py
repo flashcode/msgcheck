498fcea5a5726b781b0753cffb572d75294aaf04
Sébastien Helleu <flashcode@flashtux.org>
2014-06-28 10:23:20 +0200

    Add missing empty line in long description

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ MsgCheck performs various checks on gettext files:
 
 * compilation (with command `msgfmt -c`)
 * for each translation:
+
   - number of lines in translated strings
   - whitespace at beginning/end of strings
   - trailing whitespace at end of lines inside strings
